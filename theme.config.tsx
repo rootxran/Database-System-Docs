3d5b85963b644ef8056000109a06fd078518241a
rootxran <raoalinawaz.cys@gmail.com>
2023-03-08 20:10:11 +0500

    project initialized

---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Database System</span>,
+  project: {
+    link: 'https://github.com/rootxran/Database-System-Docs',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  footer: {
+    text: 'Database System Docs',
+  },
+}
+
+export default config
