abf2d2ec356502222929e79f1d90c28855a8dc49
rootxran <raoalinawaz.cys@gmail.com>
2023-04-16 16:32:51 +0500

    erd and data dictionary

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b035cb2f43fe76cf"; }
+/******/ 		__webpack_require__.h = function() { return "125fb89670ab51d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
